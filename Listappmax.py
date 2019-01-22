b1cdbe938543d83db9d134a5504623b50c1c4242
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-22 23:21:50 +0530

    Create Listappmax.py

---
+++ Listappmax.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+k=list(map(int,input().split()))
+c=0
+for i in range(0,len(k)):
+    l.append(k[i])
+    m=max(l)
+    c+=1
+    if c==0:
+        print(m,end="")
+    else:
+        print("",m,end="")
+        
+    
