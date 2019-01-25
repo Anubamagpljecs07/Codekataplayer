c627a9231feae3f63130638d310f8efb8ceef070
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-25 16:05:16 +0500

    Update Stringdiffbyk.py

--- Stringdiffbyk.py
+++ Stringdiffbyk.py
@@ -1,12 +1,11 @@
-r,s=map(str,input().split())
-k=int(input())
+r,s,k=map(str,input().split())
 c=0
 for i in range(0,len(r)):
     for j in range(0,len(s)):
         if i==j:
             if r[i]!=s[j]:
                 c+=1
-if c==k:
+if c==int(k):
     print("yes")
 else:
     print("no")
