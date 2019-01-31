2782f8b6950411138def832881640c8b688880f8
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-31 19:32:54 +0530

    Update Common_of_list.py

--- Common_of_list.py
+++ Common_of_list.py
@@ -2,8 +2,9 @@ n=int(input())
 l=list(map(int,input().split()))
 k=list(map(int,input().split()))
 g=[]
-for i in l:
-    for j in k:
-        if i==j:
-            g.append(i)
+for i in range(0,len(l)):
+    for j in range(0,len(k)):
+            if l[i]==k[j]:
+                if l[i] not in g:
+                    g.append(l[i])
 print(*g)
