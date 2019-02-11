078cc9bfc1c559f8c9b0553f505c058efcc05851
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-11 22:47:59 +0530

    Create Additional.py

---
+++ Additional.py
@@ -0,0 +1,21 @@
+s=input()
+r=input()
+g=[]
+if (s.isalpha() or " " in s) and (r.isalpha() or " " in r):
+    s=list(s.split(" "))
+    r=list(r.split(" "))
+    for i in s:
+        if s.count(i) > r.count(i) and i not in g:
+            g.append(i)
+    for i in r:
+        if r.count(i)>s.count(i) and i not in g:
+            g.append(i)
+    print(*g)
+else:
+    for i in s:
+        if s.count(i)>r.count(i) and i not in g:
+            g.append(i)
+    for i in r:
+        if r.count(i)>s.count(i) and i not in g:
+            g.append(i)
+    print(*g)
