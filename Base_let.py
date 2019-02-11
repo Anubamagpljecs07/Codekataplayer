ec0eaf03a83bb520d2cc5806b7d48d7ee522e33f
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-11 23:50:27 +0530

    Create Base_let.py

---
+++ Base_let.py
@@ -0,0 +1,17 @@
+r,s=map(str,input().split())
+g=[]
+h=[]
+for i in r:
+    if i not in g:
+        g.append(i)
+for i in s:
+    if i not in h:
+        h.append(i)
+c=0
+for i in g:
+    if g.count(i)==h.count(i):
+        c+=1
+if c==len(g) and c==len(h):
+    print("true")
+else:
+    print("false")
