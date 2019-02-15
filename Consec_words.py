004e71778265458d88d3f4f70a6a24aa6d701912
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-15 16:23:15 +0530

    Create Consec_words.py

---
+++ Consec_words.py
@@ -0,0 +1,16 @@
+n=int(input())
+g=[]
+c=0
+for i in range(n):
+	g.append(input())
+for i in range(0,len(g)):
+	if i<len(g)-1 and g[i]==g[i+1]:
+		print(g[i])
+		c+=1
+	elif i==len(g)-1 and g[i]==g[i-1]:
+		print(g[i-1])
+		c+=1
+if c>=1:
+	print("yes")
+else:
+	print("no")
