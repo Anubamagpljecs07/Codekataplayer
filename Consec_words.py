f7835e7217f040fd90d104193d9a0e0cae816c47
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-15 16:59:15 +0530

    Update Consec_words.py

--- Consec_words.py
+++ Consec_words.py
@@ -2,15 +2,13 @@ n=int(input())
 g=[]
 c=0
 for i in range(n):
-	g.append(input())
+    g.append(input())
 for i in range(0,len(g)):
-	if i<len(g)-1 and g[i]==g[i+1]:
-		print(g[i])
-		c+=1
-	elif i==len(g)-1 and g[i]==g[i-1]:
-		print(g[i-1])
-		c+=1
+    if i<len(g)-1 and g[i]==g[i+1]:
+        c+=1
+    elif i==len(g)-1 and g[i]==g[i-1]:
+        c+=1
 if c>=1:
-	print("yes")
+    print("yes")
 else:
-	print("no")
+    print("no")
