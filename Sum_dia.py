92f19f12146d8ba3ab67c99e9f85b606d729b72f
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-17 11:03:55 +0530

    Create Sum_dia.py

---
+++ Sum_dia.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=[]
+g=[]
+s,f=1,1
+a=0
+for i in range(0,n):
+    l.append(input().split())
+for i in range(0,n):
+    s=s*int(l[i][i])
+g.append(s)
+for i in range(0,n):
+    x=n-i-1
+    f=f*int(l[i][x])
+g.append(f)
+for i in g:
+    a=a+int(i)
+print(a)
