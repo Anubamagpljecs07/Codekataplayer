feea9f733c9e4cc4b1737a858d43fed098900e87
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-02-17 11:26:42 +0530

    Update K_attendance.py

--- K_attendance.py
+++ K_attendance.py
@@ -1,8 +1,11 @@
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
+g=[]
 if k in l:
     print(k)
 else:
     for i in l:
         if i<k:
-            print(i)
+            g.append(i)
+    print(max(g))
+    
